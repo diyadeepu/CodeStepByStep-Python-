c89775d50f2fdf5c40ba869fc0f85844fada4eab
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-23 17:21:39 -0800

    Added grade count problem

---
+++ parameters_and_return/grade_count.py
@@ -0,0 +1,22 @@
+# Write the result of each expression. Make sure to give a value of the 
+# appropriate type (such as including a .0 at the end of a float). Note that a 
+# variable's value changes only if you re-assign it using the = operator.
+# grade = 2.7
+# count = 25
+# round(grade)                        # a) grade = _____
+# grade = round(grade)                # b) grade = _____
+# mini = min(grade, math.floor(2.9))  # c) mini  = _____
+# x = math.pow(2, 4)                  # d) x     = _____
+# x = math.sqrt(64)                   # e) x     = _____
+# math.sqrt(count)                    # f) count = _____
+# count = math.sqrt(count)            # g) count = _____
+# a = abs(min(-1, -3))                # h) a     = _____
+
+# a) grade = 2.7
+# b) grade = 3.0
+# c) mini = 2.0
+# d) x = 16.0
+# e) x = 8.0
+# f) count = 25
+# g) count = 5
+# h) a = 3
