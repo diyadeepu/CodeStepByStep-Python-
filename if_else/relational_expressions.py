739d8f4df49be0f5225b15db7da884306af224c0
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-27 12:11:48 -0800

    Added relational_expressions problem

---
+++ if_else/relational_expressions.py
@@ -0,0 +1,38 @@
+# Given the following variable declarations:
+# x = 4 
+# y = -3
+# z = 4
+# What are the results of the following relational expressions?
+
+"""
+START OF ANSWER
+
+Q: x == 4
+A: true
+
+Q: x == y
+A: false
+
+Q: x == z
+A: true
+
+Q: y == z
+A: false
+
+Q: x + y > 0
+A: true
+
+Q: x - z != 0
+A: false
+
+Q: y * y <= z
+A: false
+
+Q: y // y == 1
+A: true
+
+Q: x * (y + 2) > y - (y + z) * 2
+A: true
+
+END OF ANSWER
+"""
