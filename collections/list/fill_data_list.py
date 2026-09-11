d6b10ae8c14128315d3423d81e9f8901cac64ac5
Diya Deepu <130609161+diyadeepu@users.noreply.github.com>
2026-09-11 09:39:49 +0530

    Implement fill_data_list.py with expected values
    Added expected output values for data list after execution.

---
+++ collections/list/fill_data_list.py
@@ -0,0 +1,19 @@
+# Fill in the list with the values that would be stored after the code executes:
+
+# data = [0] * 8
+# data[0] = 3
+# data[7] = -18
+# data[4] = 5
+# data[1] = data[0]
+# x = data[4]
+# data[4] = 6
+# data[x] = data[0] * data[1]
+
+data[0] = 3
+data[1] = 3
+data[2] = 0
+data[3] = 0
+data[4] = 6
+data[5] = 9
+data[6] = 0
+data[7] = -18
