5c03e5794b12a6c0f350af8921fc05b8f7c74fd3
Diya Deepu <130609161+diyadeepu@users.noreply.github.com>
2026-09-11 09:53:09 +0530

    Add is_palindrome function for list validation
    Implement is_palindrome function to check if a list of strings is a palindrome.

---
+++ collections/list/is_palindrome.py
@@ -0,0 +1,11 @@
+# Write a function is_palindrome that accepts a list of strings as its argument and 
+# returns True if that list is a palindrome (if it reads the same forwards as backwards) 
+# and False if not. For example, the list ["alpha", "beta", "gamma", "delta", "gamma", "beta", "alpha"] 
+# is a palindrome, so passing that list to your function would return True. 
+# Lists with zero or one element are considered to be palindromes.
+
+def is_palindrome(str_list):
+    for i in range (len(str_list)):
+        if str_list[len(str_list) - i - 1] != str_list[i]:
+            return False
+    return True
