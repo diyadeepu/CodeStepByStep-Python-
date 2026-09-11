32121b7209d9354794420bfd7bc0718d031552d8
Diya Deepu <130609161+diyadeepu@users.noreply.github.com>
2026-09-11 09:53:50 +0530

    Fix formatting for is_palindrome function

--- collections/list/is_palindrome.py
+++ collections/list/is_palindrome.py
@@ -1,6 +1,6 @@
-# Write a function is_palindrome that accepts a list of strings as its argument and 
-# returns True if that list is a palindrome (if it reads the same forwards as backwards) 
-# and False if not. For example, the list ["alpha", "beta", "gamma", "delta", "gamma", "beta", "alpha"] 
+# Write a function is_palindrome that accepts a list of strings as its argument and returns 
+# True if that list is a palindrome (if it reads the same forwards as backwards) and False if not. 
+# For example, the list ["alpha", "beta", "gamma", "delta", "gamma", "beta", "alpha"] 
 # is a palindrome, so passing that list to your function would return True. 
 # Lists with zero or one element are considered to be palindromes.
 
