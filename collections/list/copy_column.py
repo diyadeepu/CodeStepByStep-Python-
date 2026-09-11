733d798796bc00c68a6abbca721fd52df596f730
Diya Deepu <130609161+diyadeepu@users.noreply.github.com>
2026-09-11 09:26:11 +0530

    Implement fragment code in copy_column.py
    Add a loop to copy the second column to the fifth column in a matrix.

---
+++ collections/list/copy_column.py
@@ -0,0 +1,6 @@
+# Assume that a two-dimensional rectangular list of integers called
+# matrix has been declared with six rows and eight columns. 
+# Write a for loop to copy the contents of the second column into the fifth column.
+
+for i in range (0, 6):
+    matrix[i][4] = matrix[i][1]
