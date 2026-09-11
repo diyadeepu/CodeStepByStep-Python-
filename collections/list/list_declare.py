4a12731cdd7631a9e1fcbe8dc013573767bc1f04
Diya Deepu <130609161+diyadeepu@users.noreply.github.com>
2026-09-11 10:03:58 +0530

    Add list declaration syntax examples in comments
    Added a comment block with multiple choice options for declaring a list of integers.

---
+++ collections/list/list_declare.py
@@ -0,0 +1,12 @@
+# Which of the following choices is the correct syntax for quickly declaring 
+# / initializing a list of six integers named a to store a particular list of values?
+
+# syntax
+# • a[6] = [17, -3, 42, 5, 9, 28]
+# • a {17, -3, 42, 5, 9, 28
+# • a = [17, -3, 42, 5, 9, 28]
+# • [17, -3, 42, 5, 9, 28] = a
+# • list a [17, -3, 42, 5, 9, 28]
+
+Correct Solution:
+a = [17, -3, 42, 5, 9, 28]
