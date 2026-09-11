85afdd6189ea7c2242de33c427ef0be8533453dd
Diya Deepu <130609161+diyadeepu@users.noreply.github.com>
2026-09-11 10:04:47 +0530

    Initialize elements in list a1 with specific values
    Set specific values for elements in list a1 after mystery1 function execution.

---
+++ collections/list/list_mystery1.py
@@ -0,0 +1,14 @@
+# What are the values of the elements in list a1 after the following code executes?
+
+# def mystery1(a1, a2):
+#     for i in range(0, len(a1)): 
+#         a1[i] += a2[len(a2) - i - 1]
+# a1 = [1, 3, 5, 7, 9]
+# a2 = [1, 4, 9, 16, 25]
+# mystery1(a1, a2)    
+
+a1[0] = 26
+a1[1] = 19
+a1[2] = 14
+a1[3] = 11
+a1[4] = 10
